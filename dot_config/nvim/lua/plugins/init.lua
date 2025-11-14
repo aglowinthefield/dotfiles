5a2756ba697afbed616365c5a06e5d856329b271
aglowinthefield <146008217+aglowinthefield@users.noreply.github.com>
2025-11-14 17:40:10 -0500

    Add chezmoi nvim

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -4,5 +4,14 @@ return {
   "tpope/vim-commentary",
   "tpope/vim-surround",
   { "nvim-tree/nvim-web-devicons",     opts = {} },
-  { "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate" }
+  { "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate" },
+  {
+    'alker0/chezmoi.vim',
+    lazy = false,
+    init = function()
+      -- This option is required.
+      vim.g['chezmoi#use_tmp_buffer'] = true
+      -- add other options here if needed.
+    end,
+  },
 }
