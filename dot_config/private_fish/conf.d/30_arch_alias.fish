cbfb8dcfa134ff432bef4d8ebca1990c46e628dd
aglowinthefield <146008217+aglowinthefield@users.noreply.github.com>
2026-03-12 23:36:14 -0400

    Remove 'bat' alias and add another lsd space

--- dot_config/private_fish/conf.d/30_arch_alias.fish
+++ dot_config/private_fish/conf.d/30_arch_alias.fish
@@ -3,4 +3,3 @@ alias pacman "sudo pacman"
 alias zfsmochi='zfs list -r -o name,mountpoint,used,avail,compressratio,compression,atime,exec kura'
 alias kura="cd /media ; cat kura.txt"
 alias ls=lsd
-alias cat=bat
